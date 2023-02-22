9385d52775f8ac1bec2486007f6897993deb99e8
Edon <edon@valdman.works>
2023-02-22 10:10:01 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftUITriangle
+
+A description of this package.
---
+++ Sources/SwiftUITriangle/SwiftUITriangle.swift
@@ -0,0 +1,6 @@
+public struct SwiftUITriangle {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
